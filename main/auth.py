3d36d078c41d6117745e4f4dd6a327edd9d1b59c
Lipis <lipiridis@gmail.com>
2012-05-13 12:16:14 -0200

    Added admin_required decorator in the auth.py

--- main/auth.py
+++ main/auth.py
@@ -89,6 +89,16 @@ def login_required(f):
   return decorated_function
 
 
+def admin_required(f):
+  @functools.wraps(f)
+  def decorated_function(*args, **kws):
+    if is_logged_in() and current_user_db().admin:
+      return f(*args, **kws)
+    else:
+      flask.abort(401)
+  return decorated_function
+
+
 ################################################################################
 # Login stuff
 ################################################################################
