5c9ad1a0179660d717c0783b06d1f9fdc87e7e03
Lipis <lipiridis@gmail.com>
2012-09-29 11:09:08 -0200

    Updated pubnub.js to 3.1

--- main/static/lib/pubnub.js
+++ main/static/lib/pubnub.js
@@ -1,7 +1,474 @@
-(function(){function p(b,a){if(b&&a)if(typeof b[0]!="undefined")for(var e=0,g=b.length;e<g;)a.call(b[e],b[e],e++);else for(e in b)b.hasOwnProperty&&b.hasOwnProperty(e)&&a.call(b[e],e,b[e])}function u(b,a){var e=[];p(b||[],function(g,j){e.push(a(g,j))});return e}function q(b){return u(encodeURIComponent(b).split(""),function(a){return"-_.!~*'()".indexOf(a)<0?a:"%"+a.charCodeAt(0).toString(16).toUpperCase()}).join("")}function k(b){var a,e=function(){if(!j){j=1;clearTimeout(l);try{response=JSON.parse(a.responseText)}catch(f){return d(1)}c(response)}},
-g=0,j=0,l=setTimeout(function(){d(1)},v),r=b.fail||function(){},c=b.success||function(){},d=function(f){if(!g){g=1;clearTimeout(l);if(a){a.onerror=a.onload=null;a.abort&&a.abort();a=null}f&&r()}};try{a=typeof XDomainRequest!=="undefined"&&new XDomainRequest||new XMLHttpRequest;a.onerror=a.onabort=function(){d(1)};a.onload=a.onloadend=e;a.timeout=v;a.open("GET",b.url.join(z),true);a.send()}catch(h){d(0);return k(b)}return d}function s(b){var a={},e=b.publish_key||"",g=b.subscribe_key||"",j=b.ssl?"s":
-"",l="http"+j+"://"+(b.origin||"pubsub.pubnub.com"),r={history:function(c,d){d=c.callback||d;var h=c.limit||100,f=c.channel;f&&d&&k({url:[l,"history",g,q(f),0,h],success:function(i){d(i)},fail:function(i){d(i)}})},time:function(c){k({url:[l,"time",0],success:function(d){c(d[0])},fail:function(){c(0)}})},uuid:function(c){k({url:["http"+j+"://pubnub-prod.appspot.com/uuid"],success:function(d){c(d[0])},fail:function(){c(0)}})},publish:function(c,d){d=d||c.callback||function(){};var h=c.message,f=c.channel;
-if(h)if(f)if(e){h=JSON.stringify(h);h=[l,"publish",e,g,0,q(f),0,q(h)];k({success:function(i){d(i)},fail:function(){d([0,"Disconnected"])},url:h})}},unsubscribe:function(c){c=c.channel;if(c in a){a[c].connected=0;a[c].done&&a[c].done(0)}},subscribe:function(c,d){function h(){if(a[f].connected)a[f].done=k({callback:0,url:[A,"subscribe",g,q(f),0,w],fail:function(){if(!m){m=1;B()}setTimeout(h,1E3);r.time(function(n){if(n&&m){m=0;x()}n||C()})},success:function(n){if(a[f].connected){if(!y){y=1;D()}if(m){m=
-0;x()}i=t.set(g+f,w=i&&t.get(g+f)||n[1]);p(n[0],function(E){d(E,n)});setTimeout(h,10)}}})}var f=c.channel;d=d||c.callback;var i=c.restore,w=0,C=c.error||function(){},D=c.connect||function(){},x=c.reconnect||function(){},B=c.disconnect||function(){},m=0,y=0,A=F(l);if(f)if(d)if(g){f in a||(a[f]={});if(!a[f].connected){a[f].connected=1;h()}}},xdr:k,db:t,each:p,map:u,events:o};return r}var t=function(){var b=typeof localStorage!="undefined"&&localStorage;return{get:function(a){try{if(b)return b.getItem(a);
-if(document.cookie.indexOf(a)==-1)return null;return((document.cookie||"").match(RegExp(a+"=([^;]+)"))||[])[1]||null}catch(e){}},set:function(a,e){try{if(b)return b.setItem(a,e)&&0;document.cookie=a+"="+e+"; expires=Thu, 1 Aug 2030 20:00:00 UTC; path=/"}catch(g){}}}}(),z="/",v=31E4,F=function(){var b=Math.floor(Math.random()*9)+1;return function(a){return a.indexOf("pubsub")>0&&a.replace("pubsub","ps"+(++b<10?b:b=1))||a}}(),o={list:{},unbind:function(b){o.list[b]=[]},bind:function(b,a){(o.list[b]=
-o.list[b]||[]).push(a)},fire:function(b,a){p(o.list[b]||[],function(e){e(a)})}};typeof module!=="undefined"&&(module.exports=s)||typeof exports!=="undefined"&&(exports.PN=s)||(PUBNUB=s)})();
+/* ---------------------------------------------------------------------------
+--------------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------------
+PubNub Real-time Cloud-Hosted Push API and Push Notification Client Frameworks
+Copyright (c) 2011 PubNub Inc.
+http://www.pubnub.com/
+http://www.pubnub.com/terms
+--------------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------------
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+--------------------------------------------------------------------------- */
+
+(function(){
+
+/**
+ * UTILITIES
+ */
+function unique() { return'x'+ ++NOW+''+(+new Date) }
+function rnow() { return+new Date }
+
+/**
+ * LOCAL STORAGE
+ */
+var db = (function(){
+    var ls = typeof localStorage != 'undefined' && localStorage;
+    return {
+        get : function(key) {
+            try {
+                if (ls) return ls.getItem(key);
+                if (document.cookie.indexOf(key) == -1) return null;
+                return ((document.cookie||'').match(
+                    RegExp(key+'=([^;]+)')
+                )||[])[1] || null;
+            } catch(e) { return }
+        },
+        set : function( key, value ) {
+            try {
+                if (ls) return ls.setItem( key, value ) && 0;
+                document.cookie = key + '=' + value +
+                    '; expires=Thu, 1 Aug 2030 20:00:00 UTC; path=/';
+            } catch(e) { return }
+        }
+    };
+})();
+
+/**
+ * UTIL LOCALS
+ */
+var NOW    = 1
+,   URLBIT = '/'
+,   XHRTME = 310000;
+
+/**
+ * NEXTORIGIN
+ * ==========
+ * var next_origin = nextorigin();
+ */
+var nextorigin = (function() {
+    var ori = Math.floor(Math.random() * 9) + 1;
+    return function(origin) {
+        return origin.indexOf('pubsub') > 0
+            && origin.replace(
+             'pubsub', 'ps' + (++ori < 10 ? ori : ori=1)
+            ) || origin;
+    }
+})();
+
+/**
+ * EACH
+ * ====
+ * each( [1,2,3], function(item) { console.log(item) } )
+ */
+function each( o, f ) {
+    if ( !o || !f ) return;
+
+    if ( typeof o[0] != 'undefined' )
+        for ( var i = 0, l = o.length; i < l; )
+            f.call( o[i], o[i], i++ );
+    else
+        for ( var i in o )
+            o.hasOwnProperty    &&
+            o.hasOwnProperty(i) &&
+            f.call( o[i], i, o[i] );
+}
+
+/**
+ * MAP
+ * ===
+ * var list = map( [1,2,3], function(item) { return item + 1 } )
+ */
+function map( list, fun ) {
+    var fin = [];
+    each( list || [], function( k, v ) { fin.push(fun( k, v )) } );
+    return fin;
+}
+
+/**
+ * GREP
+ * ====
+ * var list = grep( [1,2,3], function(item) { return item % 2 } )
+ */
+function grep( list, fun ) {
+    var fin = [];
+    each( list || [], function(l) { fun(l) && fin.push(l) } );
+    return fin
+}
+
+/**
+ * timeout
+ * =======
+ * timeout( function(){}, 100 );
+ */
+function timeout( fun, wait ) {
+    return setTimeout( fun, wait );
+}
+
+/**
+ * ENCODE
+ * ======
+ * var encoded_path = encode('path');
+ */
+function encode(path) {
+    return map( (encodeURIComponent(path)).split(''), function(chr) {
+        return "-_.!~*'()".indexOf(chr) < 0 ? chr :
+               "%"+chr.charCodeAt(0).toString(16).toUpperCase()
+    } ).join('');
+}
+
+/**
+ * LOG
+ * ===
+ * var list = grep( [1,2,3], function(item) { return item % 2 } )
+ */
+var log = function(){};
+
+
+/**
+ * EVENTS
+ * ======
+ * PUBNUB.events.bind( 'you-stepped-on-flower', function(message) {
+ *     // Do Stuff with message
+ * } );
+ *
+ * PUBNUB.events.fire( 'you-stepped-on-flower', "message-data" );
+ * PUBNUB.events.fire( 'you-stepped-on-flower', {message:"data"} );
+ * PUBNUB.events.fire( 'you-stepped-on-flower', [1,2,3] );
+ *
+ */
+var events = {
+    'list'   : {},
+    'unbind' : function( name ) { events.list[name] = [] },
+    'bind'   : function( name, fun ) {
+        (events.list[name] = events.list[name] || []).push(fun);
+    },
+    'fire' : function( name, data ) {
+        each(
+            events.list[name] || [],
+            function(fun) { fun(data) }
+        );
+    }
+};
+
+/**
+ * CORS XHR Request
+ * ================
+ *  xdr({
+ *     url     : ['http://www.blah.com/url'],
+ *     success : function(response) {},
+ *     fail    : function() {}
+ *  });
+ */
+function xdr( setup ) {
+    var xhr
+    ,   finished = function() {
+            if (loaded) return;
+                loaded = 1;
+
+            clearTimeout(timer);
+
+            try       { response = JSON['parse'](xhr.responseText); }
+            catch (r) { return done(1); }
+
+            success(response);
+        }
+    ,   complete = 0
+    ,   loaded   = 0
+    ,   timer    = timeout( function(){done(1)}, XHRTME )
+    ,   fail     = setup.fail    || function(){}
+    ,   success  = setup.success || function(){}
+    ,   done     = function(failed) {
+            if (complete) return;
+                complete = 1;
+
+            clearTimeout(timer);
+
+            if (xhr) {
+                xhr.onerror = xhr.onload = null;
+                xhr.abort && xhr.abort();
+                xhr = null;
+            }
+
+            failed && fail();
+        };
+
+    // Send
+    try {
+        xhr = typeof XDomainRequest !== 'undefined' &&
+              new XDomainRequest()  ||
+              new XMLHttpRequest();
+
+        xhr.onerror = xhr.onabort   = function(){ done(1) };
+        xhr.onload  = xhr.onloadend = finished;
+        xhr.timeout = XHRTME;
+
+        xhr.open( 'GET', setup.url.join(URLBIT), true );
+        xhr.send();
+    }
+    catch(eee) {
+        done(0);
+        return xdr(setup);
+    }
+
+    // Return 'done'
+    return done;
+}
+
+
+/* =-====================================================================-= */
+/* =-====================================================================-= */
+/* =-=========================     PUBNUB     ===========================-= */
+/* =-====================================================================-= */
+/* =-====================================================================-= */
+
+function PN(setup) {
+    var CHANNELS      = {}
+    ,   PUBLISH_KEY   = setup['publish_key']   || ''
+    ,   SUBSCRIBE_KEY = setup['subscribe_key'] || ''
+    ,   SSL           = setup['ssl'] ? 's' : ''
+    ,   ORIGIN        = 'http'+SSL+'://'+(setup['origin']||'pubsub.pubnub.com')
+    ,   SELF          = {
+        /*
+            PUBNUB.history({
+                channel  : 'my_chat_channel',
+                limit    : 100,
+                callback : function(messages) { console.log(messages) }
+            });
+        */
+        'history' : function( args, callback ) {
+            var callback = args['callback'] || callback
+            ,   limit    = args['limit'] || 100
+            ,   channel  = args['channel'];
+
+            // Make sure we have a Channel
+            if (!channel)  return log('Missing Channel');
+            if (!callback) return log('Missing Callback');
+
+            // Send Message
+            xdr({
+                url      : [
+                    ORIGIN, 'history',
+                    SUBSCRIBE_KEY, encode(channel),
+                    0, limit
+                ],
+                success  : function(response) { callback(response) },
+                fail     : function(response) { callback(response) }
+            });
+        },
+
+        /*
+            PUBNUB.time(function(time){ console.log(time) });
+        */
+        'time' : function(callback) {
+            xdr({
+                url      : [ORIGIN, 'time', 0],
+                success  : function(response) { callback(response[0]) },
+                fail     : function() { callback(0) }
+            });
+        },
+
+        /*
+            PUBNUB.uuid(function(uuid) { console.log(uuid) });
+        */
+        'uuid' : function(callback) {
+            xdr({
+                url      : [
+                    'http' + SSL + '://pubnub-prod.appspot.com/uuid'
+                ],
+                success  : function(response) { callback(response[0]) },
+                fail     : function() { callback(0) }
+            });
+        },
+
+        /*
+            PUBNUB.publish({
+                channel : 'my_chat_channel',
+                message : 'hello!'
+            });
+        */
+        'publish' : function( args, callback ) {
+            var callback = callback || args['callback'] || function(){}
+            ,   message  = args['message']
+            ,   channel  = args['channel']
+            ,   url;
+
+            if (!message)     return log('Missing Message');
+            if (!channel)     return log('Missing Channel');
+            if (!PUBLISH_KEY) return log('Missing Publish Key');
+
+            // If trying to send Object
+            message = JSON['stringify'](message);
+
+            // Create URL
+            url = [
+                ORIGIN, 'publish',
+                PUBLISH_KEY, SUBSCRIBE_KEY,
+                0, encode(channel),
+                0, encode(message)
+            ];
+
+            // Send Message
+            xdr({
+                success  : function(response) { callback(response) },
+                fail     : function() { callback([ 0, 'Disconnected' ]) },
+                url      : url
+            });
+        },
+
+        /*
+            PUBNUB.unsubscribe({ channel : 'my_chat' });
+        */
+        'unsubscribe' : function(args) {
+            var channel = args['channel'];
+
+            // Leave if there never was a channel.
+            if (!(channel in CHANNELS)) return;
+
+            // Disable Channel
+            CHANNELS[channel].connected = 0;
+
+            // Abort and Remove Script
+            CHANNELS[channel].done &&
+            CHANNELS[channel].done(0);
+        },
+
+        /*
+            PUBNUB.subscribe({
+                channel  : 'my_chat'
+                callback : function(message) { console.log(message) }
+            });
+        */
+        'subscribe' : function( args, callback ) {
+
+            var channel      = args['channel']
+            ,   callback     = callback || args['callback']
+            ,   restore      = args['restore']
+            ,   timetoken    = 0
+            ,   error        = args['error'] || function(){}
+            ,   connect      = args['connect'] || function(){}
+            ,   reconnect    = args['reconnect'] || function(){}
+            ,   disconnect   = args['disconnect'] || function(){}
+            ,   disconnected = 0
+            ,   connected    = 0
+            ,   origin       = nextorigin(ORIGIN);
+
+            // Make sure we have a Channel
+            if (!channel)       return log('Missing Channel');
+            if (!callback)      return log('Missing Callback');
+            if (!SUBSCRIBE_KEY) return log('Missing Subscribe Key');
+
+            if (!(channel in CHANNELS)) CHANNELS[channel] = {};
+
+            // Make sure we have a Channel
+            if (CHANNELS[channel].connected) return log('Already Connected');
+                CHANNELS[channel].connected = 1;
+
+            // Recurse Subscribe
+            function pubnub() {
+                // Stop Connection
+                if (!CHANNELS[channel].connected) return;
+
+                // Connect to PubNub Subscribe Servers
+                CHANNELS[channel].done = xdr({
+                    callback : 0,
+                    url      : [
+                        origin, 'subscribe',
+                        SUBSCRIBE_KEY, encode(channel),
+                        0, timetoken
+                    ],
+                    fail : function() {
+                        // Disconnect
+                        if (!disconnected) {
+                            disconnected = 1;
+                            disconnect();
+                        }
+                        timeout( pubnub, 1000 );
+                        SELF['time'](function(success){
+                            // Reconnect
+                            if (success && disconnected) {
+                                disconnected = 0;
+                                reconnect();
+                            }
+                            success || error();
+                        });
+                    },
+                    success : function(messages) {
+                        if (!CHANNELS[channel].connected) return;
+
+                        // Connect
+                        if (!connected) {
+                            connected = 1;
+                            connect();
+                        }
+
+                        // Reconnect
+                        if (disconnected) {
+                            disconnected = 0;
+                            reconnect();
+                        }
+
+                        // Restore Previous Connection Point if Needed
+                        // Also Update Timetoken
+                        restore = db.set(
+                            SUBSCRIBE_KEY + channel,
+                            timetoken = restore && db.get(
+                                SUBSCRIBE_KEY + channel
+                            ) || messages[1]
+                        );
+
+                        each( messages[0], function(msg) {
+                            callback( msg, messages );
+                        } );
+
+                        timeout( pubnub, 10 );
+                    }
+                });
+            }
+
+            // Begin Recursive Subscribe
+            pubnub();
+        },
+
+        // Expose PUBNUB Functions
+        'xdr'    : xdr,
+        'db'     : db,
+        'each'   : each,
+        'map'    : map,
+        'events' : events
+    };
+
+    return SELF;
+}
+
+typeof module  !== 'undefined' && (module.exports = PN) ||
+typeof exports !== 'undefined' && (exports.PN = PN)     || (PUBNUB = PN);
+
+})();
