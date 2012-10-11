c4db71e5a6b4a516dc561204436611fddfba0112
Lipis <lipiridis@gmail.com>
2012-10-11 17:05:43 -0200

    Removed extras and chat

--- main/config.py
+++ main/config.py
@@ -55,6 +55,5 @@ SCRIPTS = {
     'src/coffee/site/app.coffee',
     'src/coffee/site/profile.coffee',
     'src/coffee/site/admin.coffee',
-    'src/coffee/site/chat.coffee',
   ],
 }
--- main/main.py
+++ main/main.py
@@ -132,54 +132,6 @@ def user_list():
 
 
 ################################################################################
-# Extras
-################################################################################
-@app.route('/_s/extras/', endpoint='extras_service')
-@app.route('/extras/', endpoint='extras')
-def extras():
-  country = None
-  region = None
-  city = None
-  city_lat_long = None
-  if 'X-AppEngine-Country' in flask.request.headers:
-    country = flask.request.headers['X-AppEngine-Country']
-  if 'X-AppEngine-Region' in flask.request.headers:
-    region = flask.request.headers['X-AppEngine-Region']
-  if 'X-AppEngine-City' in flask.request.headers:
-    city = flask.request.headers['X-AppEngine-City']
-  if 'X-AppEngine-CityLatLong' in flask.request.headers:
-    city_lat_long = flask.request.headers['X-AppEngine-CityLatLong']
-
-  extra_info = {
-    'country': country,
-    'region': region,
-    'city': city,
-    'city_lat_long': city_lat_long,
-    'user_agent': flask.request.headers['User-Agent'],
-  }
-
-  if flask.request.path.startswith('/_s/'):
-    return flask.jsonify(extra_info)
-
-  return flask.render_template(
-      'extras.html',
-      html_class='extras',
-      title='Extras',
-      extra_info=extra_info,
-    )
-
-
-@app.route('/chat/')
-def chat():
-  return flask.render_template(
-      'chat.html',
-      title='Chat',
-      html_class='chat',
-      channel_name='chat',
-    )
-
-
-################################################################################
 # Error Handling
 ################################################################################
 @app.errorhandler(400)
