100d19b61fbf62484776703e11a2c69ee040da3c
Panayiotis Lipiridis <lipiridis@gmail.com>
2013-01-28 09:40:31 -0100

    Minor spelling change

--- main/main.py
+++ main/main.py
@@ -32,7 +32,7 @@ class ProfileUpdateForm(wtf.Form):
   name = wtf.TextField('Name', [wtf.validators.required()])
   email = wtf.TextField('Email', [
       wtf.validators.optional(),
-      wtf.validators.email("That doesn't look like an email"),
+      wtf.validators.email('That does not look like an email'),
     ])
 
 
