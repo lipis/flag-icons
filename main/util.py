9b12cab22cd1ac00ec8f097508a17d06a1dcb7aa
Panayiotis Lipiridis <lipiridis@gmail.com>
2013-03-14 15:57:32 -0100

    Added slugify function into util.py

--- main/util.py
+++ main/util.py
@@ -6,6 +6,8 @@ import flask
 from uuid import uuid4
 from datetime import datetime
 import urllib
+import re
+import unicodedata
 
 import config
 
@@ -156,6 +158,18 @@ def uuid():
   ''' Generates universal unique identifier
   '''
   return str(uuid4()).replace('-', '')
+
+
+_slugify_strip_re = re.compile(r'[^\w\s-]')
+_slugify_hyphenate_re = re.compile(r'[-\s]+')
+
+
+def slugify(value):
+  if not isinstance(value, unicode):
+    value = unicode(value)
+  value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+  value = unicode(_slugify_strip_re.sub('', value).strip().lower())
+  return _slugify_hyphenate_re.sub('-', value)
 
 
 ################################################################################
