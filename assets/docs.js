fb7a47e7aed142015f0ed1e476ecdd7177c185a4
Panayiotis Lipiridis <lipiridis@gmail.com>
2013-09-27 03:56:01 +0200

    Shitty log removed

--- assets/docs.js
+++ assets/docs.js
@@ -1,12 +1,11 @@
 window.onload = function () {
   document.getElementById('btn-bootstrap').onclick = function() {
-    console.log('shit');
     if (document.getElementById('bootstrap').rel == 'stylesheet') {
-      document.getElementById('bootstrap').rel = 'none';
+      document.getElementById('bootstrap').rel = 'styleshit';
       document.getElementById('btn-bootstrap').innerHTML = 'Enable Bootstrap';
     } else {
       document.getElementById('bootstrap').rel = 'stylesheet';
-      document.getElementById('btn-bootstrap').innerHTML = 'Disalbe Bootstrap';
+      document.getElementById('btn-bootstrap').innerHTML = 'Disable Bootstrap';
     }
   }
 }
