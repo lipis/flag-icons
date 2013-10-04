b75fed49f3c3dc534bf04091f4993e81d1e32891
Serg Baburin <serg.baburin@gmail.com>
2013-10-04 15:18:48 +0400

    Replaced xx.data.strip() by the wtform filter. Simplified assignment of ConfigUpdateForm fields.

--- main/admin.py
+++ main/admin.py
@@ -7,69 +7,53 @@ import auth
 import util
 import model
 import config
-
 from main import app
 
 
+strip_filter = lambda x: x.strip() if x else ''
+
+
 class ConfigUpdateForm(wtf.Form):
-  analytics_id = wtf.TextField('Analytics ID')
-  announcement_html = wtf.TextAreaField('Announcement HTML')
+  analytics_id = wtf.TextField('Analytics ID', filters=[strip_filter])
+  announcement_html = wtf.TextAreaField(
+    'Announcement HTML', filters=[strip_filter])
   announcement_type = wtf.SelectField('Announcement Type', choices=[
       (t, t.title()) for t in model.Config.announcement_type._choices
     ])
-  brand_name = wtf.TextField('Brand Name', [wtf.validators.required()])
-  facebook_app_id = wtf.TextField('Facebook App ID')
-  facebook_app_secret = wtf.TextField('Facebook App Secret')
-  feedback_email = wtf.TextField('Feedback Email', [
-      wtf.validators.optional(),
-      wtf.validators.email('That does not look like an email')
-    ])
-  flask_secret_key = wtf.TextField('Flask Secret Key', [
-      wtf.validators.required()
-    ])
-  twitter_consumer_key = wtf.TextField('Twitter Consumer Key')
-  twitter_consumer_secret = wtf.TextField('Twitter Consumer Secret')
-  github_client_id = wtf.TextField('GitHub Client ID')
-  github_client_secret = wtf.TextField('GitHub Client Secret')
+  brand_name = wtf.TextField(
+    'Brand Name', [wtf.validators.required()], filters=[strip_filter])
+  facebook_app_id = wtf.TextField(
+    'Facebook App ID', filters=[strip_filter])
+  facebook_app_secret = wtf.TextField(
+    'Facebook App Secret', filters=[strip_filter])
+  feedback_email = wtf.TextField(
+    'Feedback Email',
+    [wtf.validators.optional(),
+     wtf.validators.email('That does not look like an email')],
+    filters=[strip_filter])
+  flask_secret_key = wtf.TextField(
+    'Flask Secret Key', [wtf.validators.required()], filters=[strip_filter])
+  twitter_consumer_key = wtf.TextField(
+    'Twitter Consumer Key', filters=[strip_filter])
+  twitter_consumer_secret = wtf.TextField(
+    'Twitter Consumer Secret', filters=[strip_filter])
+  github_client_id = wtf.TextField('GitHub Client ID', filters=[strip_filter])
+  github_client_secret = wtf.TextField(
+    'GitHub Client Secret', filters=[strip_filter])
 
 
 @app.route('/_s/admin/config/', endpoint='admin_config_update_service')
 @app.route('/admin/config/', methods=['GET', 'POST'])
 @auth.admin_required
 def admin_config_update():
-  form = ConfigUpdateForm()
-
   config_db = model.Config.get_master_db()
+  form = ConfigUpdateForm(obj=config_db)
   if form.validate_on_submit():
-    config_db.analytics_id = form.analytics_id.data.strip()
-    config_db.announcement_html = form.announcement_html.data.strip()
-    config_db.announcement_type = form.announcement_type.data.strip()
-    config_db.brand_name = form.brand_name.data.strip()
-    config_db.facebook_app_id = form.facebook_app_id.data.strip()
-    config_db.facebook_app_secret = form.facebook_app_secret.data.strip()
-    config_db.feedback_email = form.feedback_email.data.strip()
-    config_db.flask_secret_key = form.flask_secret_key.data.strip()
-    config_db.twitter_consumer_key = form.twitter_consumer_key.data.strip()
-    config_db.twitter_consumer_secret = form.twitter_consumer_secret.data.strip()
-    config_db.github_client_id = form.github_client_id.data.strip()
-    config_db.github_client_secret = form.github_client_secret.data.strip()
+    form.populate_obj(config_db)
     config_db.put()
     reload(config)
     app.config.update(CONFIG_DB=config_db)
     return flask.redirect(flask.url_for('welcome'))
-  if not form.errors:
-    form.analytics_id.data = config_db.analytics_id
-    form.announcement_html.data = config_db.announcement_html
-    form.announcement_type.data = config_db.announcement_type
-    form.brand_name.data = config_db.brand_name
-    form.facebook_app_id.data = config_db.facebook_app_id
-    form.facebook_app_secret.data = config_db.facebook_app_secret
-    form.feedback_email.data = config_db.feedback_email
-    form.flask_secret_key.data = config_db.flask_secret_key
-    form.twitter_consumer_key.data = config_db.twitter_consumer_key
-    form.twitter_consumer_secret.data = config_db.twitter_consumer_secret
-    form.github_client_id.data = config_db.github_client_id
-    form.github_client_secret.data = config_db.github_client_secret
 
   if flask.request.path.startswith('/_s/'):
     return util.jsonify_model_db(config_db)
