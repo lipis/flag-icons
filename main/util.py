bbbb105863a7296da29405f4a48ec7420be0406c
Chris Topaloudis <topless@gmail.com>
2013-11-01 15:34:15 +0100

    Update util.py

--- main/util.py
+++ main/util.py
@@ -197,3 +197,4 @@ def slugify(value):
 # Lambdas
 ################################################################################
 strip_filter = lambda x: x.strip() if x else ''
+email_filter = lambda x: x.lower().strip() if x else ''
