2e870bd6613bc51e9d42848f4779e3adf0bd1445
Panayiotis Lipiridis <lipiridis@gmail.com>
2013-12-10 21:58:33 -0100

    Simplified i18n.py

--- main/i18n.py
+++ main/i18n.py
@@ -1,43 +1,31 @@
-from flask.ext import wtf
-from flask import _request_ctx_stack
-from flask.ext.babel import get_locale
-from babel import support
 import os
 
-
-def translations_path():
-  module_path = os.path.abspath(__file__)
-  dirname = os.path.join(os.path.dirname(module_path), 'translations')
-  return dirname
+from babel import support
+from flask import _request_ctx_stack
+from flask.ext import babel
+from flask.ext import wtf
 
 
 def _get_translations():
-  try:
-    ctx = _request_ctx_stack.top
-    translations = getattr(ctx, 'wtforms_translations', None)
-    if translations is None:
-      translations = support.Translations.load(
-        translations_path(), [get_locale()], domain='messages'
-      )
-      ctx.wtforms_translations = translations
-    return translations
-  except:
-    return None
+  ctx = _request_ctx_stack.top
+  root = os.path.dirname(os.path.abspath(__file__))
+  translations_path = os.path.join(root, 'translations')
+  translations = support.Translations.load(
+      translations_path, [babel.get_locale()], domain='messages'
+    )
+  ctx.wtforms_translations = translations
+  return translations
 
 
 class Translations(object):
   def gettext(self, string):
     t = _get_translations()
-    if t is None:
-      return string
-    return t.ugettext(string)
+    return string if t is None else t.ugettext(string)
 
   def ngettext(self, singular, plural, n):
     t = _get_translations()
     if t is None:
-      if n == 1:
-        return singular
-      return plural
+      return singular if n == 1 else plural
     return t.ungettext(singular, plural, n)
 
 
