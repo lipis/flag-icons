5458923de816d466acf98fac554f3751b5593f12
Panayiotis Lipiridis <lipiridis@gmail.com>
2014-01-31 23:05:40 -0100

    Changed value to text in slugify

--- main/util.py
+++ main/util.py
@@ -191,12 +191,12 @@ _slugify_strip_re = re.compile(r'[^\w\s-]')
 _slugify_hyphenate_re = re.compile(r'[-\s]+')
 
 
-def slugify(value):
-  if not isinstance(value, unicode):
-    value = unicode(value)
-  value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
-  value = unicode(_slugify_strip_re.sub('', value).strip().lower())
-  return _slugify_hyphenate_re.sub('-', value)
+def slugify(text):
+  if not isinstance(text, unicode):
+    text = unicode(text)
+  text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore')
+  text = unicode(_slugify_strip_re.sub('', text).strip().lower())
+  return _slugify_hyphenate_re.sub('-', text)
 
 
 def is_valid_username(username):
