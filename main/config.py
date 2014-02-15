4e231be76f075ae810f2d7fb3f83b92e9f810ddc
Panayiotis Lipiridis <lipiridis@gmail.com>
2014-02-15 17:14:01 -0100

    Added CURRENT_APP_NAME to config.py

--- main/config.py
+++ main/config.py
@@ -7,13 +7,16 @@ try:
   # also used in the run.py script which is used to compile/minify the client
   # side files (*.less, *.coffee, *.js) and is not aware of the GAE
   from datetime import datetime
+  from google.appengine.api import app_identity
   import model
+
   CONFIG_DB = model.Config.get_master_db()
   SECRET_KEY = CONFIG_DB.flask_secret_key.encode('ascii')
   CURRENT_VERSION_ID = os.environ.get('CURRENT_VERSION_ID')
   CURRENT_VERSION_NAME = CURRENT_VERSION_ID.split('.')[0]
   CURRENT_VERSION_TIMESTAMP = long(CURRENT_VERSION_ID.split('.')[1]) >> 28
   CURRENT_VERSION_DATE = datetime.fromtimestamp(CURRENT_VERSION_TIMESTAMP)
+  CURRENT_APP_NAME = app_identity.get_application_id()
 except:
   pass
 
