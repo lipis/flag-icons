7fb896ff562485253fb8231ae3e193c5a4a5c452
Lipis <lipis@users.noreply.github.com>
2014-02-24 01:27:06 +0100

    Constant name changes and more

--- run.py
+++ run.py
@@ -15,85 +15,73 @@ from main import config
 ###############################################################################
 # Options
 ###############################################################################
-parser = argparse.ArgumentParser()
-parser.add_argument(
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument(
     '-w', '--watch', dest='watch', action='store_true',
     help='watch files for changes when running the development web server',
   )
-parser.add_argument(
+PARSER.add_argument(
     '-c', '--clean', dest='clean', action='store_true',
     help='''recompiles files when running the development web server, but
     obsolete if -s is used''',
   )
-parser.add_argument(
+PARSER.add_argument(
     '-m', '--minify', dest='minify', action='store_true',
     help='compiles files into minified version before deploying'
   )
-parser.add_argument(
+PARSER.add_argument(
     '-s', '--start', dest='start', action='store_true',
     help='starts the dev_appserver.py with storage_path pointing to temp',
   )
-parser.add_argument(
+PARSER.add_argument(
     '-o', '--host', dest='host', action='store', default='127.0.0.1',
     help='the host to start the dev_appserver.py',
   )
-parser.add_argument(
+PARSER.add_argument(
     '-p', '--port', dest='port', action='store', default='8080',
     help='the port to start the dev_appserver.py',
   )
-parser.add_argument(
+PARSER.add_argument(
     '-f', '--flush', dest='flush', action='store_true',
     help='clears the datastore, blobstore, etc',
   )
-args = parser.parse_args()
+ARGS = PARSER.parse_args()
 
 
 ###############################################################################
 # Directories
 ###############################################################################
 DIR_MAIN = 'main'
-DIR_STATIC = 'static'
-DIR_SRC = 'src'
 DIR_STYLE = 'style'
 DIR_SCRIPT = 'script'
-DIR_MIN = 'min'
-DIR_DST = 'dst'
-DIR_LIB = 'lib'
 DIR_NODE_MODULES = 'node_modules'
-DIR_BIN = '.bin'
 DIR_TEMP = 'temp'
-DIR_STORAGE = 'storage'
 
-FILE_ZIP = '%s.zip' % DIR_LIB
-FILE_COFFEE = 'coffee'
-FILE_LESS = 'lessc'
-FILE_UGLIFYJS = 'uglifyjs'
-FILE_GRUNT = 'grunt'
+DIR_STATIC = os.path.join(DIR_MAIN, 'static')
 
-dir_static = os.path.join(DIR_MAIN, DIR_STATIC)
+DIR_SRC = os.path.join(DIR_STATIC, 'src')
+DIR_SRC_SCRIPT = os.path.join(DIR_SRC, DIR_SCRIPT)
+DIR_SRC_STYLE = os.path.join(DIR_SRC, DIR_STYLE)
 
-dir_src = os.path.join(dir_static, DIR_SRC)
-dir_src_script = os.path.join(dir_src, DIR_SCRIPT)
-dir_src_style = os.path.join(dir_src, DIR_STYLE)
+DIR_DST = os.path.join(DIR_STATIC, 'dst')
+DIR_DST_STYLE = os.path.join(DIR_DST, DIR_STYLE)
+DIR_DST_SCRIPT = os.path.join(DIR_DST, DIR_SCRIPT)
 
-dir_dst = os.path.join(dir_static, DIR_DST)
-dir_dst_style = os.path.join(dir_dst, DIR_STYLE)
-dir_dst_script = os.path.join(dir_dst, DIR_SCRIPT)
+DIR_MIN = os.path.join(DIR_STATIC, 'min')
+DIR_MIN_STYLE = os.path.join(DIR_MIN, DIR_STYLE)
+DIR_MIN_SCRIPT = os.path.join(DIR_MIN, DIR_SCRIPT)
 
-dir_min = os.path.join(dir_static, DIR_MIN)
-dir_min_style = os.path.join(dir_min, DIR_STYLE)
-dir_min_script = os.path.join(dir_min, DIR_SCRIPT)
+DIR_LIB = os.path.join(DIR_MAIN, 'lib')
+FILE_LIB = os.path.join(DIR_MAIN, 'lib.zip')
 
-dir_lib = os.path.join(DIR_MAIN, DIR_LIB)
-file_lib = os.path.join(DIR_MAIN, FILE_ZIP)
+DIR_BIN = os.path.join(DIR_NODE_MODULES, '.bin')
+FILE_BOWER = os.path.join(DIR_BIN, 'bower')
+FILE_COFFEE = os.path.join(DIR_BIN, 'coffee')
+FILE_GRUNT = os.path.join(DIR_BIN, 'grunt')
+FILE_LESS = os.path.join(DIR_BIN, 'lessc')
+FILE_UGLIFYJS = os.path.join(DIR_BIN, 'uglifyjs')
 
-dir_bin = os.path.join(DIR_NODE_MODULES, DIR_BIN)
-file_coffee = os.path.join(dir_bin, FILE_COFFEE)
-file_less = os.path.join(dir_bin, FILE_LESS)
-file_uglifyjs = os.path.join(dir_bin, FILE_UGLIFYJS)
-file_grunt = os.path.join(dir_bin, FILE_GRUNT)
-
-dir_storage = os.path.join(DIR_TEMP, DIR_STORAGE)
+DIR_STORAGE = os.path.join(DIR_TEMP, 'storage')
 
 
 ###############################################################################
@@ -137,9 +125,9 @@ def merge_files(source, target):
   fout.close()
 
 
-def os_execute(executable, args, source, target, append=False):
+def os_execute(executable, ARGS, source, target, append=False):
   operator = '>>' if append else '>'
-  os.system('"%s" %s %s %s %s' % (executable, args, source, operator, target))
+  os.system('"%s" %s %s %s %s' % (executable, ARGS, source, operator, target))
 
 
 def compile_script(source, target_dir):
@@ -147,7 +135,7 @@ def compile_script(source, target_dir):
     print_out('NOT FOUND', source)
     return
 
-  target = source.replace(dir_src_script, target_dir).replace('.coffee', '.js')
+  target = source.replace(DIR_SRC_SCRIPT, target_dir).replace('.coffee', '.js')
   if not is_dirty(source, target):
     return
   make_dirs(os.path.dirname(target))
@@ -156,7 +144,7 @@ def compile_script(source, target_dir):
     shutil.copy(source, target)
     return
   print_out('COFFEE', source)
-  os_execute(file_coffee, '-cp', source, target)
+  os_execute(FILE_COFFEE, '-cp', source, target)
 
 
 def compile_style(source, target_dir, check_modified=False):
@@ -164,14 +152,14 @@ def compile_style(source, target_dir, check_modified=False):
     print_out('NOT FOUND', source)
     return
 
-  target = source.replace(dir_src_style, target_dir).replace('.less', '.css')
+  target = source.replace(DIR_SRC_STYLE, target_dir).replace('.less', '.css')
   minified = ''
   if not source.endswith('.less'):
     return
   if check_modified and not is_style_modified(target):
     return
 
-  if target_dir == dir_min_style:
+  if target_dir == DIR_MIN_STYLE:
     minified = '-x'
     target = target.replace('.css', '.min.css')
     print_out('LESS MIN', source)
@@ -179,15 +167,15 @@ def compile_style(source, target_dir, check_modified=False):
     print_out('LESS', source)
 
   make_dirs(os.path.dirname(target))
-  os_execute(file_less, minified, source, target)
+  os_execute(FILE_LESS, minified, source, target)
 
 
 def make_lib_zip(force=False):
-  if force and os.path.isfile(file_lib):
-    os.remove(file_lib)
-  if not os.path.isfile(file_lib):
-    print_out('ZIP', file_lib)
-    shutil.make_archive(dir_lib, 'zip', dir_lib)
+  if force and os.path.isfile(FILE_LIB):
+    os.remove(FILE_LIB)
+  if not os.path.isfile(FILE_LIB):
+    print_out('ZIP', FILE_LIB)
+    shutil.make_archive(DIR_LIB, 'zip', DIR_LIB)
 
 
 def is_dirty(source, target):
@@ -197,7 +185,7 @@ def is_dirty(source, target):
 
 
 def is_style_modified(target):
-  for folder, folders, files in os.walk(dir_src):
+  for folder, folders, files in os.walk(DIR_SRC):
     for file_ in files:
       path = os.path.join(folder, file_)
       if path.endswith('.less') and is_dirty(path, target):
@@ -207,10 +195,10 @@ def is_style_modified(target):
 
 def compile_all_dst():
   for source in config.STYLES:
-    compile_style(os.path.join(dir_static, source), dir_dst_style, True)
+    compile_style(os.path.join(DIR_STATIC, source), DIR_DST_STYLE, True)
   for module in config.SCRIPTS:
     for source in config.SCRIPTS[module]:
-      compile_script(os.path.join(dir_static, source), dir_dst_script)
+      compile_script(os.path.join(DIR_STATIC, source), DIR_DST_SCRIPT)
 
 
 def update_path_separators():
@@ -227,13 +215,15 @@ def update_path_separators():
 
 def install_dependencies():
   missing = False
-  if not os.path.exists(file_coffee):
+  if not os.path.exists(FILE_BOWER):
     missing = True
-  if not os.path.exists(file_less):
+  if not os.path.exists(FILE_COFFEE):
     missing = True
-  if not os.path.exists(file_uglifyjs):
+  if not os.path.exists(FILE_LESS):
     missing = True
-  if not os.path.exists(os.path.join(DIR_NODE_MODULES, 'grunt')):
+  if not os.path.exists(FILE_UGLIFYJS):
+    missing = True
+  if not os.path.exists(FILE_GRUNT):
     missing = True
   try:
     file_package = os.path.join(DIR_NODE_MODULES, 'uglify-js', 'package.json')
@@ -246,12 +236,12 @@ def install_dependencies():
 
   if missing:
     os.system('npm install')
-  os.system('"%s" bower' % file_grunt)
+  os.system('"%s" bower' % FILE_GRUNT)
 
 
 def update_missing_args():
-  if args.start:
-    args.clean = True
+  if ARGS.start:
+    ARGS.clean = True
 
 
 def uniq(seq):
@@ -269,54 +259,54 @@ install_dependencies()
 update_missing_args()
 
 if len(sys.argv) == 1:
-  parser.print_help()
+  PARSER.print_help()
   sys.exit(1)
 
-if args.clean:
+if ARGS.clean:
   print_out('CLEAN')
   clean_files()
   make_lib_zip(force=True)
-  remove_dir(dir_dst)
-  make_dirs(dir_dst)
+  remove_dir(DIR_DST)
+  make_dirs(DIR_DST)
   compile_all_dst()
   print_out('DONE')
 
-if args.minify:
+if ARGS.minify:
   print_out('MINIFY')
   clean_files()
   make_lib_zip(force=True)
-  remove_dir(dir_min)
-  make_dirs(dir_min_script)
+  remove_dir(DIR_MIN)
+  make_dirs(DIR_MIN_SCRIPT)
 
   for source in config.STYLES:
-    compile_style(os.path.join(dir_static, source), dir_min_style)
+    compile_style(os.path.join(DIR_STATIC, source), DIR_MIN_STYLE)
 
   for module in config.SCRIPTS:
     scripts = uniq(config.SCRIPTS[module])
     coffees = ' '.join([
-        os.path.join(dir_static, script)
+        os.path.join(DIR_STATIC, script)
         for script in scripts if script.endswith('.coffee')
       ])
 
-    pretty_js = os.path.join(dir_min_script, '%s.js' % module)
-    ugly_js = os.path.join(dir_min_script, '%s.min.js' % module)
+    pretty_js = os.path.join(DIR_MIN_SCRIPT, '%s.js' % module)
+    ugly_js = os.path.join(DIR_MIN_SCRIPT, '%s.min.js' % module)
     print_out('COFFEE MIN', ugly_js)
 
     if len(coffees):
-      os_execute(file_coffee, '--join -cp', coffees, pretty_js, append=True)
+      os_execute(FILE_COFFEE, '--join -cp', coffees, pretty_js, append=True)
     for script in scripts:
       if not script.endswith('.js'):
         continue
-      script_file = os.path.join(dir_static, script)
+      script_file = os.path.join(DIR_STATIC, script)
       merge_files(script_file, pretty_js)
-    os_execute(file_uglifyjs, pretty_js, '-cm', ugly_js)
+    os_execute(FILE_UGLIFYJS, pretty_js, '-cm', ugly_js)
     os.remove(pretty_js)
   print_out('DONE')
 
-if args.watch:
+if ARGS.watch:
   print_out('WATCHING')
   make_lib_zip()
-  make_dirs(dir_dst)
+  make_dirs(DIR_DST)
 
   compile_all_dst()
   print_out('DONE', 'and watching for changes (Ctrl+C to stop)')
@@ -326,14 +316,14 @@ if args.watch:
     update_path_separators()
     compile_all_dst()
 
-if args.flush:
-  remove_dir(dir_storage)
+if ARGS.flush:
+  remove_dir(DIR_STORAGE)
   print_out('STORAGE CLEARED')
 
-if args.start:
-  make_dirs(dir_storage)
-  clear = 'yes' if args.flush else 'no'
-  port = int(args.port)
+if ARGS.start:
+  make_dirs(DIR_STORAGE)
+  clear = 'yes' if ARGS.flush else 'no'
+  port = int(ARGS.port)
   run_command = '''
       dev_appserver.py %s
       --host %s
@@ -342,5 +332,5 @@ if args.start:
       --storage_path=%s
       --clear_datastore=%s
       --skip_sdk_update_check
-    ''' % (DIR_MAIN, args.host, port, port + 1, dir_storage, clear)
+    ''' % (DIR_MAIN, ARGS.host, port, port + 1, DIR_STORAGE, clear)
   os.system(run_command.replace('\n', ' '))
