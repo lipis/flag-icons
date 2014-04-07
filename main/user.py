e4d586a6079fe7e5d6773aec8e7730ef20202f63
Chris Topaloudis <topless@gmail.com>
2014-04-07 15:34:00 +0200

    Removed unnecessary name param in query

--- main/user.py
+++ main/user.py
@@ -25,7 +25,6 @@ def user_list():
       limit=util.param('limit', int),
       cursor=util.param('cursor'),
       order=util.param('order') or '-created',
-      name=util.param('name'),
       admin=util.param('admin', bool),
       active=util.param('active', bool),
       permissions=util.param('permissions', list),
