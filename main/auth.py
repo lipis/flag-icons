d5aec068223270204452be95dafa23b1c61277f7
Panayiotis Lipiridis <lipiridis@gmail.com>
2014-09-03 05:20:34 -0200

    After signing out redirect to sign in

--- main/auth.py
+++ main/auth.py
@@ -179,7 +179,7 @@ def signin():
 def signout():
   login.logout_user()
   flask.flash(u'You have been signed out.', category='success')
-  return flask.redirect(flask.url_for('welcome'))
+  return flask.redirect(flask.url_for('signin'))
 
 
 ###############################################################################
