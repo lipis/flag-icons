6f86cec47eff0e80a63a9fd5500d1fba13e79ac9
Panayiotis Lipiridis <lipiridis@gmail.com>
2014-09-05 15:49:13 -0200

    URL list is used in get_next_url

--- main/util.py
+++ main/util.py
@@ -40,7 +40,10 @@ def param(name, cast=None):
 def get_next_url(next_url=''):
   next_url = next_url or param('next') or param('next_url')
   if next_url:
-    if flask.url_for('signin') in next_url:
+    do_not_redirect_urls = [
+        flask.url_for('signin'),
+      ]
+    if any(url in next_url for url in do_not_redirect_urls):
       return flask.url_for('welcome')
     return next_url
   referrer = flask.request.referrer
