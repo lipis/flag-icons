22c0d0a4635dbe43a249769fe6ea9b63434c61db
Chris Topaloudis <topless@gmail.com>
2014-10-22 18:43:22 +0200

    Added user_forgot to not_redirect_urls

--- main/util.py
+++ main/util.py
@@ -41,12 +41,7 @@ def param(name, cast=None):
 def get_next_url(next_url=''):
   next_url = next_url or param('next') or param('next_url')
   if next_url:
-    do_not_redirect_urls = [
-        flask.url_for('signin'),
-        flask.url_for('signup'),
-        flask.url_for('user_reset'),
-      ]
-    if any(url in next_url for url in do_not_redirect_urls):
+    if any(url in next_url for url in config.DO_NOT_REDIRECT_URLS):
       return flask.url_for('welcome')
     return next_url
   referrer = flask.request.referrer
