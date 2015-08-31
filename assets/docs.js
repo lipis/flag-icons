f1494330c22a5c9526ea57087fbc2335264d7f19
Panayiotis Lipiridis <lipiridis@gmail.com>
2015-08-31 03:07:13 +0200

    Preview on click

--- assets/docs.js
+++ assets/docs.js
@@ -8,4 +8,16 @@ window.onload = function () {
       document.getElementById('btn-bootstrap').innerHTML = document.getElementById('btn-bootstrap').innerHTML.replace('Enable', 'Disable');
     }
   }
+
+  $('.all-flags .flag-icon-background').click(function(event){
+    var flag = $(event.currentTarget).attr('title');
+    var w = 640;
+    var h = 480;
+    var left = (screen.width / 2) - (w / 2);
+    var top = (screen.height / 2) - (h / 2);
+
+    if (flag) {
+      window.open('/flags/4x3/' + flag + '.svg', 'flag-4x3', 'width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+    }
+  });
 }
