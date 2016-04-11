5fbe0b42dd7cc3253361d3bb3ee7037fe5494ec0
Panayiotis Lipiridis <lipiridis@gmail.com>
2016-04-12 00:11:27 +0200

    Pre-release version 4.0.3

--- main/__init__.py
+++ main/__init__.py
@@ -6,11 +6,8 @@ gae-init
 Google App Engine with Bootstrap, Flask and tons of other cool features.
 
 https://github.com/gae-init
-http://gae-init.appspot.com
-
-Copyright (c) 2012-2015 by Panayiotis Lipiridis.
-License MIT, see LICENSE for more details.
+https://gae-init.appspot.com
 
 """
 
-__version__ = '4.0.2'
+__version__ = '4.0.3'
