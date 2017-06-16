abdf915303d3f3891adb0c3d8c4b73a377cb57cc
Serg Baburin <serg.baburin@gmail.com>
2017-06-16 13:47:54 +0300

    Fix #603 (#685)

--- main/appengine_config.py
+++ main/appengine_config.py
@@ -3,8 +3,11 @@
 import os
 import sys
 
+from path_util import sys_path_insert
+
+
 if os.environ.get('SERVER_SOFTWARE', '').startswith('Google App Engine'):
-  sys.path.insert(0, 'lib.zip')
+  sys_path_insert('lib.zip')
 else:
   if os.name == 'nt':
     os.name = None
@@ -16,9 +19,9 @@ else:
   re_ = stubs.FakeFile._skip_files.pattern.replace('|^lib/.*', '')
   re_ = re.compile(re_)
   stubs.FakeFile._skip_files = re_
-  sys.path.insert(0, 'lib')
-sys.path.insert(0, 'libx')
+  sys_path_insert('lib')
 
+sys_path_insert('libx')
 
 def webapp_add_wsgi_middleware(app):
   from google.appengine.ext.appstats import recording
