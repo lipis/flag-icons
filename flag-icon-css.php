e493a32d6b7d553d79211b78ca6c5287be9b22e9
Bas Bloemsaat <bas@bloemsaat.com>
2019-01-13 14:05:28 +0100

    got it working

--- flag-icon-css.php
+++ flag-icon-css.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Flag Icon CSS
-Version: 0.0.1
+Version: 0.0.5
 Author: Bas Bloemsaat
 Description: Adds flag-icon-css to wordpress
 */
@@ -12,14 +12,12 @@ defined( 'ABSPATH' ) or die( 'No direct access' );
 if ( ! class_exists( 'Flag_Icon_CSS' ) ) :
     class Flag_Icon_CSS {
         public function __construct() {
-            add_action( 'wp_enqueue_scripts', array( $this, 'register_styles' ) );
-        }
+            wp_enqueue_style( 'flag-icon-css', plugins_url( 'css/flag-icon.min.css', __FILE__ ) );
 
-        public static function register_styles( $atts ) {
-            wp_register_style( 'flag-icon-css', plugins_url( 'css/flag-icon.min.css', __FILE__ ) );
         }
-
     }
 
+    $flag_icon_css = new Flag_Icon_CSS;
 
+endif;
     
