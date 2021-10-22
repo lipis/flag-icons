a7c9c9d97efe99b2ec62663938205e617290bd78
Lipis <lipiridis@gmail.com>
2021-10-22 23:26:29 +0300

    Update homepage (#861)

--- assets/docs.js
+++ assets/docs.js
@@ -1,13 +1,56 @@
-window.onload = function () {
-  $('.all-flags .flag-icon-background').click(function(event){
-    const flag = $(event.currentTarget).attr('title');
-    const w = 640;
-    const h = 480;
-    const left = window.outerWidth / 2 + window.screenX - ( w / 2);
-    const top = window.outerHeight / 2 + window.screenY - ( h / 2);
+loadJSON = (path, callback) => {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType('application/json');
+  xobj.open('GET', path, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == '200') {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+};
 
-    if (flag) {
-      window.open('flags/4x3/' + flag + '.svg', 'flag-4x3', 'width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+window.onload = function () {
+  const flagsRow = document.getElementById('flags');
+  console.log('divvvv', flagsRow);
+  loadJSON('country.json', response => {
+    const countries = JSON.parse(response);
+    console.log(countries[3].name);
+
+    for (country of countries) {
+      console.log(country);
+      const colDiv = document.createElement('div');
+      colDiv.classList.add('col-xl-2');
+      colDiv.classList.add('col-lg-3');
+      colDiv.classList.add('col-md-4');
+      colDiv.classList.add('col-6');
+      const flagDiv = document.createElement('div');
+      flagDiv.classList.add('flag');
+
+      // Code
+      const codeSpan = document.createElement('span');
+      codeSpan.classList.add('flag-code');
+      const code = document.createTextNode(
+        country.flag_4x3.substr(10).replace('.svg', ''),
+      );
+      codeSpan.appendChild(code);
+
+      //Country
+      const countryDiv = document.createElement('div');
+      countryDiv.classList.add('flag-country');
+      countryDiv.classList.add('no-wrap');
+      countryDiv.title = country.name;
+      const countryName = document.createTextNode(country.name);
+      countryDiv.appendChild(codeSpan);
+      countryDiv.appendChild(countryName);
+      const flagImg = document.createElement('img');
+      flagImg.classList.add('flag-img');
+      flagImg.src = country.flag_4x3;
+
+      colDiv.appendChild(flagDiv);
+      flagDiv.appendChild(countryDiv);
+      flagDiv.appendChild(flagImg);
+      flagsRow.appendChild(colDiv);
     }
   });
-}
+};
