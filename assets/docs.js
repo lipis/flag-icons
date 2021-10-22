8069044450c1bee36b90e37e53719fdd37a29343
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-10-22 23:02:19 +0300

    Updates

--- assets/docs.js
+++ assets/docs.js
@@ -20,24 +20,32 @@ window.onload = function () {
     for (country of countries) {
       console.log(country);
       const colDiv = document.createElement('div');
+      colDiv.classList.add('col-xl-2');
       colDiv.classList.add('col-lg-3');
       colDiv.classList.add('col-md-4');
-      colDiv.classList.add('col-sm-6');
+      colDiv.classList.add('col-6');
       const flagDiv = document.createElement('div');
       flagDiv.classList.add('flag');
       const countryDiv = document.createElement('div');
       countryDiv.classList.add('flag-country');
-      countryDiv.classList.add('no-wrap')
+      countryDiv.classList.add('no-wrap');
       const countryName = document.createTextNode(country.name);
+      countryDiv.appendChild(countryName);
       const flagImg = document.createElement('img');
       flagImg.classList.add('flag-img');
       flagImg.src = country.flag_4x3;
-      countryDiv.appendChild(countryName);
+      // Code
+      const codeDiv = document.createElement('div');
+      codeDiv.classList.add('flag-code');
+      const code = document.createTextNode(
+        country.flag_4x3.substr(10).replace('.svg', ''),
+      );
+      codeDiv.appendChild(code);
 
-      colDiv.appendChild(flagDiv)
+      colDiv.appendChild(flagDiv);
       flagDiv.appendChild(countryDiv);
       flagDiv.appendChild(flagImg);
-      console.log(flagDiv);
+      flagDiv.appendChild(codeDiv);
       flagsRow.appendChild(colDiv);
     }
   });
