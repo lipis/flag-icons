8c5266c7cb0e640750f2a74fa668010b6a38bb34
Tyler Dunn <ireallylikeshrek@gmail.com>
2021-10-22 04:32:24 -0400

    Example: Misc updates (#846)
    * Example: Revert es-ca to es-ct
    
    Fixes 1dda087cee478cdd4d180dbdbab5a6f2fba9e70a
    
    * Example: Remove dead Google+ link
    
    * Example: Bump libraries to latest minor versions
    
    * Example: Account for multiple screens when opening flag
    
    This makes sure the flag is opened up in the center of the screen,
    even in multi-screen scenarios.
    
    var is also switched to const.

--- assets/docs.js
+++ assets/docs.js
@@ -1,10 +1,10 @@
 window.onload = function () {
   $('.all-flags .flag-icon-background').click(function(event){
-    var flag = $(event.currentTarget).attr('title');
-    var w = 640;
-    var h = 480;
-    var left = (screen.width / 2) - (w / 2);
-    var top = (screen.height / 2) - (h / 2);
+    const flag = $(event.currentTarget).attr('title');
+    const w = 640;
+    const h = 480;
+    const left = window.outerWidth / 2 + window.screenX - ( w / 2);
+    const top = window.outerHeight / 2 + window.screenY - ( h / 2);
 
     if (flag) {
       window.open('flags/4x3/' + flag + '.svg', 'flag-4x3', 'width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
