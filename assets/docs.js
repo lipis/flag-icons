07a395549a4d8967f7c584ffe39b27f2a12da056
Panayiotis Lipiridis <lipiridis@gmail.com>
2022-06-23 19:07:53 +0300

    button to switch

--- assets/docs.js
+++ assets/docs.js
@@ -40,24 +40,79 @@ const addFlag = (country, rowDiv) => {
   countryDiv.appendChild(codeSpan);
   countryDiv.appendChild(dividerSpan);
   countryDiv.appendChild(countrySpan);
+
   const flagImg = document.createElement("img");
   flagImg.classList.add("flag-img");
   flagImg.src = country.flag_4x3;
   flagImg.alt = `Flag of ${country.name}`;
 
+  const flagImgSquare = document.createElement("img");
+  flagImgSquare.classList.add("flag-img-square");
+  flagImgSquare.classList.add("hide");
+  flagImgSquare.src = country.flag_1x1;
+  flagImgSquare.alt = `Flag of ${country.name}`;
+
   colDiv.appendChild(flagDiv);
   flagDiv.appendChild(countryDiv);
   flagDiv.appendChild(flagImg);
+  flagDiv.appendChild(flagImgSquare);
   rowDiv.appendChild(colDiv);
+};
+
+const show4x3 = () => {
+  const click4x3 = document.getElementById("click-4x3");
+  const click1x1 = document.getElementById("click-1x1");
+  click1x1.classList.remove("hide");
+  click4x3.classList.add("hide");
+  const flags = document.getElementsByClassName("flag-img");
+  for (flag of flags) {
+    console.log(flag);
+    flag.classList.remove("hide");
+  }
+  const flagsSquared = document.getElementsByClassName("flag-img-square");
+  for (flag of flagsSquared) {
+    console.log(flag);
+    flag.classList.add("hide");
+  }
+};
+
+const show1x1 = () => {
+  const click4x3 = document.getElementById("click-4x3");
+  const click1x1 = document.getElementById("click-1x1");
+  click4x3.classList.remove("hide");
+  click1x1.classList.add("hide");
+  const flagsSquared = document.getElementsByClassName("flag-img-square");
+  for (flag of flagsSquared) {
+    console.log(flag);
+    flag.classList.remove("hide");
+  }
+  const flags = document.getElementsByClassName("flag-img");
+  for (flag of flags) {
+    console.log(flag);
+    flag.classList.add("hide");
+  }
 };
 
 window.onload = function () {
   const isoFlagsRow = document.getElementById("iso-flags");
   const nonIsoFlagsRow = document.getElementById("non-iso-flags");
+  const click4x3 = document.getElementById("click-4x3");
+  click4x3.addEventListener("click", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    show4x3();
+  });
+
+  const click1x1 = document.getElementById("click-1x1");
+  click1x1.addEventListener("click", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    show1x1();
+  });
+
   loadJSON("country.json", (response) => {
     const countries = JSON.parse(response);
     for (country of countries) {
-      console.log(country);
       if (country.iso) {
         addFlag(country, isoFlagsRow);
       } else {
